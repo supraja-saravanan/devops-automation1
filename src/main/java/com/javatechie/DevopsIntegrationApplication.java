09840c9d6be840513e69bb8294946696dd1eae69
supraja-saravanan <32225146+supraja-saravanan@users.noreply.github.com>
2023-09-06 17:57:17 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to SUPU WORLD";
+		return "welcome to SUPUS WORLD";
 	}
 
 	public static void main(String[] args) {
